52eb9519713c2e8940226e00fc3f0ce8e42e0321
JoelStell <jjs757@stern.nyu.edu>
2017-07-10 20:03:21 -0400

    Print the number of products

--- groceries.py
+++ groceries.py
@@ -21,8 +21,11 @@ products = [
     {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
     {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-]   
+]
 
-print(products)
+print("--------------")
+print("THERE ARE " + str(len(products)) + " PRODUCTS:")
 
-# code.interact(local=locals())
+
+
+#code.interact(local=locals())
